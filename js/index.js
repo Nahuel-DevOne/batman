10b4e29a2ef5f2b60de275ba94b93f15f997d1b7
Nahuel <nahuel.profesor@gmail.com>
2023-03-03 08:52:47 -0300

    pattern module added

--- js/index.js
+++ js/index.js
@@ -1,42 +1,59 @@
-/** Variables */
-let linternaApagada = true;
-/** Selectores */
-const sonidoBatman = document.querySelector('#bati_encendido');
-const sonidoBoton = document.querySelector('#bati_click');
-const batman = document.querySelector('#batman');
-const batmanBoton = document.querySelector('#bati_boton');
+/** Agregando patrón módulo: IIFE (Immediately Invoked Function Expression) */
+/** Agrega seguridad al código, evitando el acceso a las funciones desde afuera */
+const llamadoAyuda = (() => {
+    "use strict";
 
-/** Funciones */
-const controlarLinterna = () => {
-    if (linternaApagada) {
-        // se enciende la luz
-        linternaApagada = false;
-        batman.classList.add('batman_activo');
-
-    } else {
-        // se apaga la luz
-        linternaApagada = true;
-        batman.classList.remove('batman_activo');
+    /** Variables */
+    let linternaApagada = true;
+    /** Selectores */
+    const sonidoBatman = document.querySelector('#bati_encendido');
+    const sonidoBoton = document.querySelector('#bati_click');
+    const batman = document.querySelector('#batman');
+    const batmanBoton = document.querySelector('#bati_boton');
+    
+    /** Funciones */
+    const controlarLinterna = () => {
+        if (linternaApagada) {
+            // se enciende la luz
+            linternaApagada = false;
+            batman.classList.add('batman_activo');
+    
+        } else {
+            // se apaga la luz
+            linternaApagada = true;
+            batman.classList.remove('batman_activo');
+        }
     }
-}
-
-const sonidoLinterna = () => {
-    if (sonidoBatman.paused) {
-        // se enciende la batiseñal
-        sonidoBoton.play();
-        sonidoBatman.play();
-    } else {
-        // se apaga la batiseñal
-        sonidoBoton.play();
-        sonidoBatman.pause();
-        sonidoBatman.currentTime=0;
+    
+    const sonidoLinterna = () => {
+        if (sonidoBatman.paused) {
+            // se enciende la batiseñal
+            sonidoBoton.play();
+            sonidoBatman.play();
+        } else {
+            // se apaga la batiseñal
+            sonidoBoton.play();
+            sonidoBatman.pause();
+            sonidoBatman.currentTime=0;
+        }
     }
-}
+    
+    const aplicarFunciones = () => {
+        controlarLinterna();
+        sonidoLinterna();
+    }
 
-/** Eventos */
-batman.addEventListener('click', () => {
-    controlarLinterna();
-    sonidoLinterna();
-})
+    /** Eventos */
+    // batman.addEventListener('click', () => {
+    //     controlarLinterna();
+    //     sonidoLinterna();
+    // })
+    
+    return {
+        inicializarAuxilio: aplicarFunciones
+    }
+
+})();
+
 
 
