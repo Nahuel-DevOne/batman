a076961f77b9f04baea5a9108262afc9e98901dd
Nahuel <nahuel.profesor@gmail.com>
2023-03-03 00:49:26 -0300

    refactored code

--- js/index.js
+++ js/index.js
@@ -1,28 +1,37 @@
 /** Variables */
 let linternaApagada = true;
-
 /** Selectores */
 const sonidoBatman = document.querySelector('#bati_encendido');
 const sonidoBoton = document.querySelector('#bati_click');
 const batman = document.querySelector('#batman');
 const batmanBoton = document.querySelector('#bati_boton');
 
-/** Funciones */
+// Funciones
 const controlarLinterna = () => {
+    if (linternaApagada) {
+        linternaApagada = false;
+        // sonido
+        batman.classList.add('batman_activo');
 
-    linternaApagada ? 
-        (linternaApagada = false && batman.classList.add('batman_activo')) 
-        : (linternaApagada = true && batman.classList.remove('batman_activo'))
+    } else {
+        linternaApagada = true;
+        //sonido
+        batman.classList.remove('batman_activo');
+    }
 }
 
 const sonidoLinterna = () => {
-
-    sonidoBatman.paused ? 
-        (sonidoBoton.play() && sonidoBatman.play()) 
-        : (sonidoBoton.play() && sonidoBatman.pause() && (sonidoBatman.currentTime=0))
+    if (sonidoBatman.paused) {
+        sonidoBoton.play();
+        sonidoBatman.play();
+    } else {
+        sonidoBoton.play();
+        sonidoBatman.pause();
+        sonidoBatman.currentTime=0;
+    }
 }
 
-/** Eventos */
+// eventos
 batman.addEventListener('click', () => {
     controlarLinterna();
     sonidoLinterna();
